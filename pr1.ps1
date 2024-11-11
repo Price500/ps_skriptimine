0181a5ddc3584a676a529fdce2d42a27f736374f
Raini <raini.koorts@voco.ee>
2024-11-11 10:44:50 +0200

    Practice 1 - += operator

---
+++ pr1.ps1
@@ -0,0 +1,11 @@
+﻿#Define arrays
+$array1 = @(1, 2, 3)
+$array2 = @(4, 5, 6)
+#Define empty
+$array3 = @()
+#Add values
+$array3 += $array1[0] + $array2[0]
+$array3 += $array1[1] + $array2[1]
+$array3 += $array1[2] + $array2[2]
+#Print values
+$array3

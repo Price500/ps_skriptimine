acb511c997a503ad8d7ac452720f2e383aaa4a67
Raini <raini.koorts@voco.ee>
2024-11-11 10:52:24 +0200

    Practice 1 - ArrayList Collection and method

--- pr1.ps1
+++ pr1.ps1
@@ -2,10 +2,11 @@
 $array1 = @(1, 2, 3)
 $array2 = @(4, 5, 6)
 #Define empty
-$array3 = @()
+##$array3 = New-object -TypeName "System.Collections.ArrayList"
+$array3 = [System.Collections.ArrayList]@()
 #Add values
-$array3 += $array1[0] + $array2[0]
-$array3 += $array1[1] + $array2[1]
-$array3 += $array1[2] + $array2[2]
+$array3.add($array1[0] + $array2[0]) | Out-Null
+$array3.add($array1[1] + $array2[1]) | Out-Null
+$array3.add($array1[2] + $array2[2]) | Out-Null
 #Print values
 $array3

b93319b1b16e6dd0b2da6a9d9569d2f838171bdd
Raini <raini.koorts@voco.ee>
2024-11-11 11:23:16 +0200

    Using and adding hashtable elements

---
+++ pr2.ps1
@@ -0,0 +1,11 @@
+﻿#Initial data
+$worked = @{Name = "John", "Joe", "Mary"; DaysWorked = 12, 20, 18}
+$salaryday = @{Name = "John", "Joe", "Mary"; SalaryPerDay = 100, 120, 150}
+#New hashtable
+$salarytotal = [System.Collections.Hashtable]@{}
+#Calculations
+$salarytotal.add($worked.name[0], ($worked.daysworked[0] * $salaryday.salaryperday[0]))
+$salarytotal.add($worked.name[1], ($worked.daysworked[1] * $salaryday.salaryperday[1]))
+$salarytotal.add($worked.name[2], ($worked.daysworked[2] * $salaryday.salaryperday[2]))
+#Print results
+$salarytotal

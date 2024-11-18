c9e568de94bb0e7fb336bb135cd168a61c937360
Raini <raini.koorts@voco.ee>
2024-11-18 10:14:31 +0200

    Countries

---
+++ pr4.2.ps1
@@ -0,0 +1,8 @@
+﻿$riigid = @("India", "Australia", "China")
+$pealinnad = @("New Delhi", "Canberra", "Bejing")
+Write-Host "Select country" -ForegroundColor Yellow
+Write-Host "1. India" -foregroundColor Cyan
+Write-Host "2. Australia" -foregroundColor Cyan
+Write-Host "3. China" -foregroundColor Cyan
+[int32]$valik = Read-Host "Your input"
+Write-Host $riigid[$valik-1]"'s capital is "$pealinnad[$valik-1]

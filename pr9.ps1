121dc45206f96e45c5d7151668d6f3c532b90ccc
Raini <raini.koorts@voco.ee>
2024-11-18 11:20:30 +0200

    Modified

--- pr9.ps1
+++ pr9.ps1
@@ -1,7 +1,13 @@
 ﻿$c = 0
 do{
-    Write-Host "Notepad is running" -ForegroundColor DarkRed
-    $c++
-    Start-Sleep -Seconds 1}
-    while(Get-Process Notepad -ErrorAction SilentlyContinue)
+$ch = $null
+$ch = Get-Process | ?{$_.name -like "note*"}
+    if ($ch -ne $null){
+    $co = 2
+        Write-Host "Notepad is running" -ForegroundColor DarkRed
+        $c++
+        Start-Sleep -Seconds 1}
+    else{
+        $co = 1}}
+    while($co -ne 1)
 $c

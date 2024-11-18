0fdaf51c647d0218e375eed12d2649599084724d
Raini <raini.koorts@voco.ee>
2024-11-18 10:15:00 +0200

    Foreach

---
+++ pr6.ps1
@@ -0,0 +1,13 @@
+﻿$students = Import-Csv C:\Users\Raini\Desktop\pr6.csv
+$result = @()
+    foreach ($s in $students){
+        if ([int]$s.Age -gt 3 -and [int]$s.Age -lt 11){
+            $school = "Junior"}
+        else {
+            $school = "Senior"}
+        $temp = [PSCustomObject]@{
+            Name = $s.Name
+            School = $school}
+        $result += $temp}
+$result
+$result | Export-Csv -path C:\Users\Raini\Desktop\result.csv

115bc65a114f2e1769aaef80191cf8ed0f2b8960
Raini <raini.koorts@voco.ee>
2024-11-18 11:26:42 +0200

    Do-Until modified

---
+++ pr10.ps1
@@ -0,0 +1,13 @@
+﻿$c = 0
+do{
+$ch = $null
+$ch = Get-Process | ?{$_.name -like "note*"}
+    if ($ch -ne $null){
+    $co = 2
+        Write-Host "Notepad is running" -ForegroundColor DarkRed
+        $c++
+        Start-Sleep -Seconds 1}
+    else{
+        $co = 1}}
+    until($co -eq 1)
+$c

4684cf8e67bdcbdd7119eaefcc2c72aaea1de7de
Raini <raini.koorts@voco.ee>
2024-11-18 10:34:55 +0200

    For(Random)

---
+++ pr7.ps1
@@ -0,0 +1,7 @@
+﻿$result = @()
+for($i = 1 ; $i -lt 11 ; $i++){
+    $temp = [PSCustomObject]@{
+        Rollnumber = $i
+        Color = "Red", "Green", "Yellow", "Blue" | Get-Random}
+    $result += $temp}
+$result

dce656f54e763f7f09594bb441fdcde59ba18b11
Raini <raini.koorts@voco.ee>
2024-11-25 10:52:30 +0200

    Function without form(parameters)

---
+++ pr12.1.ps1
@@ -0,0 +1,5 @@
+﻿function countstart{
+    Write-Host "Total running services is"@(Get-Service | where {$_.status -eq "Running"}).Count}
+
+function countstop{
+    Write-Host "Total stopped services is"@(Get-Service | where {$_.status -eq "Stopped"}).Count}
